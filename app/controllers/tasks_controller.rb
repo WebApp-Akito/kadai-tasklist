4e1d7499051a27bd246c18a1dbcff3f1f7445629
akito.hamada <puroguraminngu05@gmail.com>
2020-06-07 07:34:59 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,21 @@
 class TasksController < ApplicationController
-   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     @tasks = Task.all
-    redirect_to controller: :users, action: :new
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
        flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
@@ -22,6 +23,7 @@ class TasksController < ApplicationController
       flash.now[:danger] = 'Taskが投稿されませんでした'
       render :new
     end
+    
   end
 
   def edit
@@ -55,5 +57,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+ 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
-
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @tasklist = current_user.tasklists.build  # form_with 用
+      @task = current_user.tasks.build  # form_with 用
       @tasklists = current_user.tasklists.order(id: :desc).page(params[:page])
     end
   end
